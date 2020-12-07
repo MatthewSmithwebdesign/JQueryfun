4c8d3f8f6f88a5e9c9fcdddd6a7140a066d1cdd2
Matthew Smith <matthewsmithwebdesign@gmail.com>
2020-12-06 16:08:20 -0800

    added jquery that changes the background color at 1000 px, 700 px then smaller than 700 px

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+$(function() {
+
+    $(window).resize(function(){
+
+    if($(window).width()>1000){
+        $('body').css
+        ({ 
+            background:'red'
+    })
+
+  } else if ($(window).width() >700) {
+      $('body').css
+      ({background: 'blue'
+    })
+
+  }else{
+      $('body').css({
+              background: 'yellow'
+        })
+  }
+  })
+});
