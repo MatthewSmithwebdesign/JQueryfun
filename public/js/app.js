d55ee72fc0f49549f335c46bfe1985d3fe8e92ea
Matthew Smith <matthewsmithwebdesign@gmail.com>
2020-12-07 17:08:19 -0800

    click on the first box and it changes the background color

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,31 @@
 $(function() {
 
+$(window).resize(function(){
+    if ($(window).width()> 1000){
+        $('body').css
+        ({background:'red'
+    })
+    }else if ($(window).width()> 700){
+        4('body').css
+        ({background: 'blue'
+    })
+    }else{
+        $('body').css
+        ({
+            background:'yellow'
+        })
+    }
+    })
+
+
+
+
+
    $('.box1').on('click', function(){
        $('body').css({
            background:'red'
        })
    })
    
+
 });
